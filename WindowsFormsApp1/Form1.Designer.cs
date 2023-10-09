52aa5a81b9e066631877fb4dd467ea273cbbc9db
hilal <130781977+mshelovee@users.noreply.github.com>
2023-10-09 10:29:26 +0300

    Add files via upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMeb = new System.Windows.Forms.TextBox();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtMeb
+            // 
+            this.txtMeb.Location = new System.Drawing.Point(46, 102);
+            this.txtMeb.Multiline = true;
+            this.txtMeb.Name = "txtMeb";
+            this.txtMeb.Size = new System.Drawing.Size(137, 22);
+            this.txtMeb.TabIndex = 0;
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.BackColor = System.Drawing.Color.OliveDrab;
+            this.btnHesapla.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnHesapla.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnHesapla.Location = new System.Drawing.Point(46, 160);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(137, 33);
+            this.btnHesapla.TabIndex = 1;
+            this.btnHesapla.Text = "button1";
+            this.btnHesapla.UseVisualStyleBackColor = false;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(247, 279);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.txtMeb);
+            this.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMeb;
+        private System.Windows.Forms.Button btnHesapla;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            string kullanici_adi;
+            kullanici_adi = txtMeb.Text;
+            if (kullanici_adi == "milliegitim @meb.k12.tr")
+ 
+                MessageBox.Show("Kullanıcı sisteme kayıtlıdır.");
+
+
+         else
+            {
+                MessageBox.Show("Kullanıcı adınız yanlıştır.");
+            }
+        }
+    }
+}
